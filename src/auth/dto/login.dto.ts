a79419eb60cad297867b3942e8da615d732d41de
Raya Dev <dev@raya-boutique.com>
2026-02-08 08:59:44 +0100

    fix: allow password min 4 chars for HTML demo compat + add Congo tenant + fix ADMIN to PDG roles

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -3,12 +3,20 @@ import { IsEmail, IsString, MinLength } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginDto {
-    @ApiProperty({ example: 'admin@raya.com', description: 'Adresse email de l utilisateur' })
-    @IsEmail({}, { message: 'Email invalide' })
-    email: string;
+  @ApiProperty({
+    example: 'admin@raya.com',
+    description: 'Adresse email de l utilisateur',
+  })
+  @IsEmail({}, { message: 'Email invalide' })
+  email: string;
 
-    @ApiProperty({ example: 'Admin123!', description: 'Mot de passe (min 6 caracteres)' })
-    @IsString()
-    @MinLength(6, { message: 'Le mot de passe doit contenir au moins 6 caracteres' })
-    password: string;
+  @ApiProperty({
+    example: 'Admin123!',
+    description: 'Mot de passe (min 4 caracteres)',
+  })
+  @IsString()
+  @MinLength(4, {
+    message: 'Le mot de passe doit contenir au moins 4 caracteres',
+  })
+  password: string;
 }
